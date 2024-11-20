5dd9b8d0dbe296bb724e56941edc6ae33d40fc4d
pebeid <48051935+pebeid@users.noreply.github.com>
2024-11-20 20:36:34 +1100

    execute with go routine (concurrency)

--- batchjob/batchjob.go
+++ batchjob/batchjob.go
@@ -2,14 +2,23 @@ package batchjob
 
 // JobResult
 type JobResult struct {
-	result  interface{}
-	err     error
-	success bool
+	Job     Job
+	Result  interface{}
+	Error   error
+	Success bool
 }
 
 // Job interface
 type Job interface {
 	Execute() (interface{}, error)
+}
+
+// endJob is a special type of job that indicates that all jobs in a batch were processed
+type endJob struct {
+}
+
+func (e *endJob) Execute() (interface{}, error) {
+	return "nil", nil
 }
 
 // BatchProcessor
@@ -44,4 +53,25 @@ func (bp *BatchProcessor) RemoveJob(job Job) bool {
 
 func (bp *BatchProcessor) Count() int {
 	return len(bp.jobCache)
+}
+
+func (bp *BatchProcessor) ExecuteImmediate(callback func(JobResult)) {
+	results := make(chan JobResult, len(bp.jobCache))
+	go func() {
+		for _, job := range bp.jobCache {
+			result, err := job.Execute()
+			results <- JobResult{Job: job, Result: result, Error: err, Success: err == nil}
+		}
+		results <- JobResult{Job: &endJob{}, Result: nil, Error: nil, Success: true}
+	}()
+	// shadow
+	for result := range results {
+		job := result.Job
+		_, isEndJob := job.(*endJob)
+		if isEndJob {
+			break
+		} else {
+			callback(result)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/pebeid/batch-processor-go/batchjob"
 	"github.com/pebeid/batch-processor-go/batchjob/makejob"
 )
 
 func main() {
 	processor, _ := batchjob.InstantiateBatchProcessor(10)
-	printHelloJob := makejob.WithNoArgsAndNoReturn(printHello)
-	printHelloJob2 := makejob.WithNoArgsAndNoReturn(printHello)
-	processor.AddJob(printHelloJob)
-	processor.AddJob(printHelloJob2)
-	println(processor.Count())
-	processor.RemoveJob(printHelloJob)
-	println(processor.Count())
+	// printHelloJob := makejob.WithNoArgsAndNoReturn(printHello)
+	// printHelloJob2 := makejob.WithNoArgsAndNoReturn(printHello)
+	// processor.AddJob(printHelloJob)
+	// processor.AddJob(printHelloJob2)
+	// println(processor.Count())
+	// processor.RemoveJob(printHelloJob)
+	// println(processor.Count())
 	// executer := batchjob.BatchJob{}
 	// executer.Execute(printHello)
+	processor.AddJob(makejob.WithNoArgs(getTimeStamp))
+	processor.AddJob(makejob.WithNoArgs(getTimeStamp))
+	processor.AddJob(makejob.WithNoArgs(getTimeStamp))
+	processor.ExecuteImmediate(func(res batchjob.JobResult) {
+		switch res.Success {
+		case true:
+			println("Count: " + strconv.Itoa(res.Result.(int)))
+		case false:
+			println("Error executing job:", res.Error)
+		}
+	})
 
 }
 
-func printHello() {
-	println("HELLO WORLD!")
+// func printHello() {
+// 	println("HELLO WORLD!")
+// }
+
+var count = 0
+
+func getTimeStamp() (interface{}, error) {
+	count++
+	return count, nil
 }
