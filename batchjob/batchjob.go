cb54776685453a7060828360688a16c466836ccf
pebeid <48051935+pebeid@users.noreply.github.com>
2024-11-20 11:28:59 +1100

    implementing job and batchprocessor

--- batchjob/batchjob.go
+++ batchjob/batchjob.go
@@ -1,8 +1,23 @@
 package batchjob
 
-type BatchJob struct {
+type Job interface {
+	Execute()
 }
 
-func (bj *BatchJob) Execute(task func()) {
-	task()
+type batchProcessorInner struct {
+	jobCache   []Job
+	batchCount int16
 }
+
+type BatchProcessor batchProcessorInner
+
+func (p BatchProcessor) Instantiate(batchCount int16) (BatchProcessor, bool) {
+	return BatchProcessor{jobCache: []Job{}, batchCount: 1}, true
+}
+
+// type Job struct {
+// }
+
+// func (bj *Job) Execute(task func()) {
+// 	task()
+// }
